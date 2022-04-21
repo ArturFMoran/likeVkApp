5ffae65d269aa4e28a82e34f2888997d024d6907
ArturFMoran <aobayojimbo@gmail.com>
2022-04-21 14:34:25 +0300

    Initial Commit

---
+++ likeVkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  likeVkApp
+//
+//  Created by Artur Felipe Moran on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
